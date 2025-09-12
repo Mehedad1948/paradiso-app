db2dd270bb4c39e9093ac26dd71c26e86141db37
Mehrdad <mnourib13@gmail.com>
2025-09-12 23:52:28 +0330

    Add loafing to get invite links modal

--- app/(pages)/(panel)/rooms/[roomId]/components/InviteByLink.tsx
+++ app/(pages)/(panel)/rooms/[roomId]/components/InviteByLink.tsx
@@ -4,8 +4,8 @@ import { useAction } from '@/hooks/core/useAction';
 import { useServerQuery } from '@/hooks/core/useServerQuery';
 import { Button } from '@heroui/button';
 import { addToast } from '@heroui/toast';
-import { Link } from 'lucide-react';
-
+import { Link, LucideSplinePointer } from 'lucide-react';
+import { Spinner } from "@heroui/spinner";
 export default function InviteByLink({ roomId }: { roomId: string }) {
     const { data, isLoading, refetch } = useServerQuery(() => getInviteLinks(roomId))
     const { data: generatedLinkData, execute, isLoading: isGenerating } = useAction(generateInviteLink, {
@@ -21,7 +21,7 @@ export default function InviteByLink({ roomId }: { roomId: string }) {
     return (
         <div className=''>
             <div className='flex flex-col gap-2'>
-                {data?.data && data?.data?.length > 0 ?
+                {isLoading ? <Spinner /> : data?.data && data?.data?.length > 0 ?
                     data?.data?.map(item => <div>{item.inviteUrl}</div>) :
                     <div onClick={() => !isGenerating && execute(roomId)}
                         className='border cursor-pointer hover:bg-primary-50 border-dashed bg-primary-50/50 border-primary-200 rounded-xl p-6 flex items-center justify-center'>
